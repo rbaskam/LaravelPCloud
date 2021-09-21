6709a065ba87b4cabdf7a89f7bd71d886f4748b8
Robert Askam <rbaskam@gmail.com>
2021-09-21 15:54:46 +0100

    first commit

---
+++ src/LaravelPCloud.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rbaskam\LaravelPCloud;
+
+class LaravelPCloud
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doding today?';
+    }
+}
